fe6117be91e21cc12b70b5eae0893108b1eb5bc8
DivyaStalin <cdivyastalin.com>
2023-05-05 13:26:22 +0530

    first commit

--- route/productroute.js
+++ route/productroute.js
@@ -8,7 +8,7 @@ router.post('/addproduct',isAdmin, async (req,res)=>{
         let productName = req.body.productName;
         let price = req.body.price;
         let color = req.body.color;
-        
+        let catUuid=req.body.catUuid;
         let userUuid = req.body.userUuid;
         
         const data = new productSchema(req.body);
@@ -60,7 +60,7 @@ router.put("/updateproduct",async (req, res) => {
         });
         }
     });
-    router.get('/getallproduct',isAdmin,async(req,res)=>{
+    router.get('/getallproduct', async(req,res)=>{
         const alluser = await productSchema.find().exec();
     
         if(alluser){
@@ -91,7 +91,7 @@ router.put("/updateproduct",async (req, res) => {
     let catName = req.body.catName;
     let image = req.body.image;
     let userUuid = req.body.userUuid;
-    let pName = req.body.pName;
+    
     const data = new catSchema(req.body);
     const result = await data.save();
     console.log("result:",result);
@@ -129,6 +129,84 @@ router.get("/getcatuser",async(req,res) => {
     }
 });
 
-    
+
+//aggregate using category and user based product
+router.get('/catuserandproduct',async(req,res)=>{
+try{
+    const details = await catSchema.aggregate([
+        {
+            $lookup:{
+            from:"Product",
+            localField:"uuid",
+            foreignField:"catUuid",
+            as:"product_details"
+        }
+
+        },
+        {
+            $lookup:{
+            from:"users",
+            localField:"userUuid",
+            foreignField:"uuid",
+            as:"user_details"
+        }
+
+        },
+        
+        {
+            $project: {
+                _id:0,
+                createdAt:0,
+                updatedAt:0,
+                image:0,
+                _v:0
+            }
+        },
+        {
+            $unwind:{
+                path:"$product_details",
+                preserveNullAndEmptyArrays: true,
+            }
+        },
+    ]);
+    if(details.length>0){
+        res.status(200).json({
+            status:true,message:"success",result:details
+        });
+    }else{
+        res.status(200).json({
+        status:true,message:"Product details are not found"
+        });
+    }
+
+}catch(err){
+  res.status(400).json({status:false,message:err.message});
+  console.log(err.message);
+}
+});
+//filter product by price
+router.get('/filterByPrice', async(req,res)=>{
+    try{
+        let id = req.query.id;
+        const filterProduct = await productSchema.find({price:id}).exec();
+        if(filterProduct)
+             res.status(200).json({status:true,message:'success',result:filterProduct});
+        }catch(err){
+        res.status(400).json({status:false,message:err.message});
+      console.log(err.message);
+    }  
+    });  
+    //sort product by name
+    router.get('/getProductByName', async(req,res)=>{
+        try{
+            let id = req.query.id;
+            const filterProduct = await productSchema.find({productName:id}).exec();
+            if(filterProduct)
+                 res.status(200).json({status:true,message:'success',result:filterProduct});
+            }catch(err){
+            res.status(400).json({status:false,message:err.message});
+          console.log(err.message);
+        }  
+});  
     
 module.exports = router;
